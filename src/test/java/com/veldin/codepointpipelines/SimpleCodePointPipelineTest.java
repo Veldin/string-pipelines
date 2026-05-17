15552a43077050507703a6c2995fc112ff01e9a8
Veldin <amr.jonkman@gmail.com>
2026-05-17 20:13:06 +0200

    Added CodePointUtils chop itest

--- src/test/java/com/veldin/codepointpipelines/SimpleCodePointPipelineTest.java
+++ src/test/java/com/veldin/codepointpipelines/SimpleCodePointPipelineTest.java
@@ -2,8 +2,7 @@ package com.veldin.codepointpipelines;
 
 import org.junit.jupiter.api.Test;
 
-import static com.veldin.codepointpipelines.ECodePointOperation.CAPITALIZE;
-import static com.veldin.codepointpipelines.ECodePointOperation.CHOMP;
+import static com.veldin.codepointpipelines.ECodePointOperation.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleCodePointPipelineTest {
@@ -21,6 +20,22 @@ class SimpleCodePointPipelineTest {
 
         assertInstanceOf(AbstractCodePointPipeline.class, simpleCodePointPipeline);
         assertEquals("This is a Simple pipeline.", result); // 'This' is capitalized.
+    }
+
+    @Test
+    void createBasicExampleChainChop() {
+
+        AbstractCodePointPipeline simpleCodePointPipeline =
+                new CodePointPipelineBuilder()
+                        .pipe(CHOP)
+                        .pipe(CHOP)
+                        .pipe(CHOP)
+                        .build();
+
+        String result = simpleCodePointPipeline.apply("1234567");
+
+        assertInstanceOf(AbstractCodePointPipeline.class, simpleCodePointPipeline);
+        assertEquals("1234", result);
     }
 
 }
